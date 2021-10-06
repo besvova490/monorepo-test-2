eacb05f7f51cd46abd5e42c4d175fece25e9d071
volodymyr-coax-dev <76474763+volodymyr-coax-dev@users.noreply.github.com>
2021-10-06 13:05:47 +0300

    test

---
+++ lib/Container.tsx
@@ -0,0 +1,12 @@
+function Container():JSX.Element {
+    return (
+        <section>
+            <h1>Hello world</h1>
+            <h2>Hello world</h2>
+            <h3>Hello world</h3>
+            <a href=""><i>Some other tesx</i></a>
+        </section>
+    );
+}
+
+export default Container;
